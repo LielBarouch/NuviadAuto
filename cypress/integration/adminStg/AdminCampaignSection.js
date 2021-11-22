940a8ff8e3c6f7738a3aa9c9b96b25985e6d6e88
cronus <lielbarouch@gmail.com>
2021-11-22 16:45:24 +0200

    New project stracture

--- cypress/integration/adminStg/AdminCampaignSection.js
+++ cypress/integration/adminStg/AdminCampaignSection.js
@@ -1,5 +1,8 @@
 /// <reference types="Cypress" />
 
+const testActorId='actor_QVAJjkxL4ldx4P6zF8DsgMKfqKQJO'
+const testActorName='Patternz'
+
 describe('Login to admin dashboard', function () {
     beforeEach(function () {
         cy.fixture('example').then(function (data) {
@@ -27,6 +30,19 @@ describe('Campaign section', function () {
         })
         cy.getToken("liel@nuviad.com", "lb123456")
     })
+    function getCampaignsApi(urlToTest) {
+        const token = Cypress.env('token');
+        const Authorization = token;
+        const apiToTest = {
+            method: 'GET',
+            url: urlToTest,
+            headers: {
+                Authorization,
+            },
+            body: {}
+        }
+        return apiToTest
+    }
     it('Enter Exchanges section', function () {
         cy.get(':nth-child(5) > .nav-link').click()
         cy.wait(3000)
@@ -36,8 +52,14 @@ describe('Campaign section', function () {
         const token = Cypress.env('token');
         const Authorization = token;
         cy.get('.css-1hwfws3').click()
-        cy.get('.css-1pahdxg-control').type('Patternz')
+        cy.get('.css-1pahdxg-control').type(testActorName)
         cy.get('.css-11unzgr').contains('Patternz (sivangrisario@gmail.com)').click()
-        cy.checkApiLoad(`${this.data.API_BASE_URL}/admin/campaigns?owner_id=actor_QVAJjkxL4ldx4P6zF8DsgMKfqKQJO`, Authorization)
+        cy.checkApiLoad(`${this.data.API_BASE_URL}/admin/campaigns?owner_id=${testActorId}`, Authorization)
+    })
+
+    it('Compare table data to with API data - All',function(){
+        cy.request(getCampaignsApi(`${this.data.API_BASE_URL}/admin/campaigns?owner_id=${testActorId}`)).then(response=>{
+            cy.log(response.body.length)
+        })
     })
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -18,110 +18,113 @@ Cypress.Commands.add('login', (email, password) => {
     cy.get('.btn').click()
 })
 
-Cypress.Commands.add('AdminLogin',(email,password)=>{
+Cypress.Commands.add('AdminLogin', (email, password) => {
     cy.get('#email').clear()
     cy.get('#password').clear()
     cy.get('#email').type(email)
     cy.get('#password').type(password)
-    
-    
+
+
 })
 
-Cypress.Commands.add('dashboardLogin',(email,password)=>{
+Cypress.Commands.add('dashboardLogin', (email, password) => {
     cy.get('input[type="email"]').clear()
     cy.get('input[type="password"]').clear()
     cy.get('input[type="email"]').type(email)
     cy.get('input[type="password"]').type(password)
 })
 
-Cypress.Commands.add('getToken',(email,password)=>{
-    
+Cypress.Commands.add('getToken', (email, password) => {
+
     cy.request({
-        method:'POST',
+        method: 'POST',
         url: 'https://api.nuviad.com/api/auth/user',
-        body:{
+        body: {
             email,
             password,
         }
-    }).as('loginResponse').then((response)=>{
-        Cypress.env('token',response.body.token);
+    }).as('loginResponse').then((response) => {
+        Cypress.env('token', response.body.token);
         return response
-    }).its('status').should('eq',200)
+    }).its('status').should('eq', 200)
 })
 
 
-Cypress.Commands.add('locationSearch',(loc)=>{
+Cypress.Commands.add('locationSearch', (loc) => {
     cy.get('.search-location-field').clear()
     cy.get('.search-location-field').type(loc)
-    cy.get('.ng-scope>.item>div>.ng-binding').should('include.text',loc)
-    cy.get('.ng-scope>.item>div>div>small').each(($el,index,$list)=>{
-        let locationsText=$el.text()
+    cy.get('.ng-scope>.item>div>.ng-binding').should('include.text', loc)
+    cy.get('.ng-scope>.item>div>div>small').each(($el, index, $list) => {
+        let locationsText = $el.text()
         expect(locationsText).to.include(loc)
     })
 })
 
-Cypress.Commands.add('checkApiLoad',(apiUrl,Authorization)=>{
-    const apiToTest={
-        method:'GET',
-            url:apiUrl,
-            headers:{
-                Authorization,
-            }
+Cypress.Commands.add('checkApiLoad', (apiUrl, Authorization) => {
+    const apiToTest = {
+        method: 'GET',
+        url: apiUrl,
+        headers: {
+            Authorization,
         }
-    cy.request(apiToTest).its('status').should('eq',200)
+    }
+    cy.request(apiToTest).its('status').should('eq', 200)
     cy.request(apiToTest).its('body').should('not.be.empty')
-    
+
 })
 
-Cypress.Commands.add('checkApiLoadDelete',(apiUrl,Authorization)=>{
-    const apiToTest={
-        method:'DELETE',
-            url:apiUrl,
-            headers:{
-                Authorization,
-            }
+Cypress.Commands.add('checkApiLoadDelete', (apiUrl, Authorization) => {
+    const apiToTest = {
+        method: 'DELETE',
+        url: apiUrl,
+        headers: {
+            Authorization,
         }
-    cy.request(apiToTest).its('status').should('eq',200)
+    }
+    cy.request(apiToTest).its('status').should('eq', 200)
 })
 
-Cypress.Commands.add('checkApiLoadPOST',(apiUrl,Authorization)=>{
-    const apiToTest={
-        method:'POST',
-            url:apiUrl,
-            headers:{
-                Authorization,
-            }
+Cypress.Commands.add('checkApiLoadPOST', (apiUrl, Authorization) => {
+    const apiToTest = {
+        method: 'POST',
+        url: apiUrl,
+        headers: {
+            Authorization,
         }
-    cy.request(apiToTest).its('status').should('eq',200)
+    }
+    cy.request(apiToTest).its('status').should('eq', 200)
     cy.request(apiToTest).its('body').should('not.be.empty')
-    
+
 })
 
-Cypress.Commands.add('checkApiLoadPUT',(apiUrl,Authorization)=>{
-    const apiToTest={
-        method:'PUT',
-            url:apiUrl,
-            headers:{
-                Authorization,
-            }
+Cypress.Commands.add('checkApiLoadPUT', (apiUrl, Authorization) => {
+    const apiToTest = {
+        method: 'PUT',
+        url: apiUrl,
+        headers: {
+            Authorization,
         }
-    cy.request(apiToTest).its('status').should('eq',200)
+    }
+    cy.request(apiToTest).its('status').should('eq', 200)
     cy.request(apiToTest).its('body').should('not.be.empty')
-    
+
 })
 
-Cypress.Commands.add('selectTableRows',(selection,selectionToCompair,tableNum,table)=>{
+Cypress.Commands.add('selectTableRows', (selection, selectionToCompair, tableNum, table) => {
     cy.get('select').eq(tableNum).select(selection)
     cy.wait(3000)
-    cy.get(table).find('tbody>tr').then((rows)=>{
-        if(selectionToCompair==100){
+    cy.get(table).find('tbody>tr').then((rows) => {
+        if (selectionToCompair == 100) {
             /* cy.wrap(rows.length).should('gt',50) */
-        }else{
-            cy.wrap(rows.length).should('be.lte',selectionToCompair)
+        } else {
+            cy.wrap(rows.length).should('be.lte', selectionToCompair)
         }
-        
+
     })
 })
+
+
+
 //
 //
 // -- This is a child command --
