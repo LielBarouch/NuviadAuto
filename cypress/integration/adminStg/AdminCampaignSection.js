1e7164285bdbee6952b4024831ed4f59e278ff80
cronus <lielbarouch@gmail.com>
2021-11-23 18:35:47 +0200

    Campaign section

--- cypress/integration/adminStg/AdminCampaignSection.js
+++ cypress/integration/adminStg/AdminCampaignSection.js
@@ -1,7 +1,9 @@
 /// <reference types="Cypress" />
-
-const testActorId='actor_QVAJjkxL4ldx4P6zF8DsgMKfqKQJO'
-const testActorName='Patternz'
+const dayjs = require('dayjs')
+const testActorId = 'actor_QVAJjkxL4ldx4P6zF8DsgMKfqKQJO'
+const testActorName = 'Patternz'
+const token = Cypress.env('token');
+const Authorization = token;
 
 describe('Login to admin dashboard', function () {
     beforeEach(function () {
@@ -48,7 +50,7 @@ describe('Campaign section', function () {
         cy.wait(3000)
         cy.url().should('eq', 'https://admin-stg.nuviad.com/dashboard/campaigns')
     })
-    it('Search for actor',function(){
+    it('Search for actor', function () {
         const token = Cypress.env('token');
         const Authorization = token;
         cy.get('.css-1hwfws3').click()
@@ -57,9 +59,48 @@ describe('Campaign section', function () {
         cy.checkApiLoad(`${this.data.API_BASE_URL}/admin/campaigns?owner_id=${testActorId}`, Authorization)
     })
 
-    it('Compare table data to with API data - All',function(){
-        cy.request(getCampaignsApi(`${this.data.API_BASE_URL}/admin/campaigns?owner_id=${testActorId}`)).then(response=>{
-            cy.log(response.body.length)
+    it('Refreshing the table', function () {
+        cy.get('.sc-bdVaJa').click()
+        cy.checkApiLoad(`${this.data.API_BASE_URL}/admin/campaigns?owner_id=${testActorId}`, Authorization)
+    })
+
+    it('Table rows display test', function () {
+        cy.selectTableRows('25', 25, 0, '#nuviad-campaigns-card')
+        cy.selectTableRows('50', 50, 0, '#nuviad-campaigns-card')
+        cy.selectTableRows('10', 10, 0, '#nuviad-campaigns-card')
+        cy.selectTableRows('100', 100, 0, '#nuviad-campaigns-card')
+    })
+
+    it('Compare table data to with API data - All', function () {
+        cy.request(getCampaignsApi(`${this.data.API_BASE_URL}/admin/campaigns?owner_id=${testActorId}`)).then(response => {
+            for (let i = 0; i < response.body.length; i++) {
+
+                cy.get('tbody > tr').then($tableRows => {
+                    for (let j = 1; j <= $tableRows.length; j++) {
+                        cy.get(`tbody > :nth-child(${j}) > :nth-child(2)`).then($id => {
+                            if ($id.text() == response.body[i].id) {
+                                cy.get(`tbody > :nth-child(${j}) > :nth-child(3)`).then($name => {
+                                    expect($name.text()).to.eq(response.body[i].name)
+                                })
+                                cy.get(`tbody > :nth-child(${j}) > :nth-child(5)`).then($status => {
+                                    expect($status.text()).to.eq(response.body[i].status)
+                                })
+                                cy.get(`tbody > :nth-child(${j}) > :nth-child(6)`).then($margin => {
+                                    expect(Number($margin.text())).to.eq(response.body[i].margin)
+                                })
+                                cy.get(`tbody > :nth-child(${j}) > :nth-child(7)`).then($spending => {
+                                    const spending = Number(response.body[i].spending) / 1000000
+                                    let spend = $spending.text()
+                                    spend = Number(spend.replace(/\$|,/g, ''))
+                                    cy.log(spending.toFixed(2) + ' ' + spend.toFixed(2))
+                                    expect(spending.toFixed(2)).to.eq(spend.toFixed(2))
+                                })
+                            }
+                        })
+                    }
+                })
+            }
         })
     })
+
 })
